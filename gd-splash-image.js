0480cf60c345a199ec5db6c6495b0d3217269867
ali <alin02912@gmail.com>
2022-03-29 19:42:51 -0400

    deleted old files

--- gd-splash-image.js
+++
@@ -1,5 +0,0 @@
-var gdjs;
-(function(gdjs2) {
-gdjs2.splashImage = blank;
-})(gdjs || (gdjs = {}));
-//# sourceMappingURL=gd-splash-image.js.map
--- pixi-renderers/pixi.js
+++
@@ -1,9 +0,0 @@
-/*!
- * pixi.js-legacy - v6.0.1
- * Compiled Wed, 24 Mar 2021 20:02:24 UTC
- *
- * pixi.js is licensed under the MIT License.
- * http://www.opensource.org/licenses/mit-license
- */
-var GlobalPIXIModule = { PIXI: PIXI };
-//# sourceMappingURL=pixi-legacy.min.js.map
